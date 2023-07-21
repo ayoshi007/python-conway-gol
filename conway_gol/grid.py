770184beee5a0ebc5535eb078aca5b63eecc22c4
ayoshi007 <89048791+ayoshi007@users.noreply.github.com>
2023-07-21 17:09:19 -0500

    Add constructor for class

--- conway_gol/grid.py
+++ conway_gol/grid.py
@@ -1,2 +1,19 @@
+import numpy as np
+from transition import step
+
 class GridOfLife:
-    pass
+    def __init__(self, m: int, n: int, starting_pattern: np.ndarray = None):
+        assert (m > 3 and n > 3)
+        self.m = m
+        self.n = n
+        self.grid = np.full((m, n), False)
+        if np.ndarray is not None:
+            assert (starting_pattern.shape[0] <= m and starting_pattern.shape[1] <= n)
+            self.__insert_pattern(starting_pattern)
+    
+    def __insert_pattern(self, pattern: np.ndarray):
+        height, width = pattern.shape
+        r, c = self.m // 2 - height // 2, self.n // 2 - width // 2
+        for i in range(height):
+            for j in range(width):
+                self.grid[r + i, c + j] = pattern[i, j]
