442d41995864952021c76d6fedb534515042f7ff
ayoshi007 <89048791+ayoshi007@users.noreply.github.com>
2023-07-21 14:57:46 -0500

    Add tests for step, made other helper functions module-private

--- conway_gol/transition.py
+++ conway_gol/transition.py
@@ -8,23 +8,23 @@ def step(state: np.ndarray, successor: np.ndarray, m: int, n: int) -> None:
     field_sum = 0
     for r in range(m):
         for c in range(n):
-            field_sum = get_field_sum(state, get_neighborhood(r, c, m, n))
+            field_sum = _get_field_sum(state, _get_neighborhood(r, c, m, n))
             successor[r, c] = field_sum == ALIVE_SUM
             if field_sum == NO_CHANGE_SUM:
                 successor[r, c] = state[r, c]
 
 
-def get_field_sum(grid: np.ndarray, neighborhood: list[int]) -> int:
+def _get_field_sum(grid: np.ndarray, neighborhood: list[int]) -> int:
     return sum([grid[r, c] for r, c in neighborhood])
 
 
-def get_neighborhood(row: int, col: int, m: int, n: int) -> list[int]:
+def _get_neighborhood(row: int, col: int, m: int, n: int) -> list[int]:
     arr = []
-    arr.append(
+    arr.extend([
         [row - 1,   col - 1],   [row - 1,   col],   [row - 1,   col + 1],
         [row,       col - 1],   [row,       col],   [row,       col + 1],
         [row + 1,   col - 1],   [row + 1,   col],   [row + 1,   col + 1]
-    )
+    ])
     arr = [
         [m - 1 if r < 0 else (0 if r == m else r),
          n - 1 if c < 0 else (0 if c == n else c)]
--- tests/test_transition.py
+++ tests/test_transition.py
@@ -1,13 +1,49 @@
-# from conway_gol.transition import step, get_field_sum, get_neighborhood
+import pytest
+import numpy as np
+from conway_gol.transition import step
 
 
-def test_step():
-    pass
+@pytest.fixture
+def get_living_state():
+	return np.array([
+		[False,	False,	True,	False],
+		[False,	True,	False,	False],
+		[False,	True,	True,	False],
+		[False,	False,	False,	False],
+	])
 
 
-def test_get_field_sum():
-    pass
+@pytest.fixture
+def get_dying_state():
+	return np.array([
+		[True,  True,   False,  True],
+		[True,  True,   False,  True],
+		[True,  True,   False,  True],
+		[True,  True,   False,  True],
+	])
 
 
-def test_get_neighborhood():
-    pass
+def test_living_step(get_living_state):
+	successor = np.empty(shape=(4, 4), dtype=np.bool_)
+	state = get_living_state
+	step(state, successor, 4, 4)
+	assert (successor == np.array([
+		[False,	False,	False,	False],
+		[False,	True,	False,	False],
+		[False,	True,	True,	False],
+		[False,	True,	True,	False],
+	])).all()
+
+
+def test_dying_step(get_dying_state):
+	successor = np.empty(shape=(4, 4), dtype=np.bool_)
+	state = get_dying_state
+	step(state, successor, 4, 4)
+	step(successor, state, 4, 4)
+	assert (successor == np.array([
+		[False,	False,	False,	False],
+		[False,	False,	False,	False],
+		[False,	False,	False,	False],
+		[False,	False,	False,	False],
+	])).all()
+	assert (state == successor).all()
