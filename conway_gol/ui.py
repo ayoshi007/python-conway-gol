aeceaa4a82c7d23502b05d88118fbccfca014f52
ayoshi007 <89048791+ayoshi007@users.noreply.github.com>
2023-07-26 10:58:55 -0500

    Add other input widgets

--- conway_gol/ui.py
+++ conway_gol/ui.py
@@ -10,7 +10,8 @@ class UIOfLife:
         menu_bar = tk.Menu(self.window)
         self.window.config(
             width=800, height=600,
-            menu=menu_bar
+            menu=menu_bar,
+            background='black'
         )
         file_menu = tk.Menu(menu_bar, tearoff=False)
         file_menu.add_command(label='Load pattern', accelerator='Ctrl+O')
@@ -24,16 +25,33 @@ class UIOfLife:
         display_menu.add_command(label='Clear', accelerator='Ctrl+C')
         menu_bar.add_cascade(label='Display', menu=display_menu)
 
-        self.canvas = tk.Canvas(self.window, width=600, height=400, bg='grey')
+        self.canvas = tk.Canvas(self.window, width=600, height=400, highlightbackground='black', highlightthickness=1)
         self.canvas.pack(anchor=tk.CENTER, expand=True)
 
         self.button_frame = tk.Frame(self.window, relief=tk.RAISED, bg='grey')
-        self.button_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=False, padx=2, pady=2)
+        self.button_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=False, padx=8, pady=2)
 
-        open_button = tk.Button(self.button_frame, text="Open")
+        open_button = tk.Button(self.button_frame, text='Open')
         open_button.pack(side=tk.RIGHT)
-        save_button = tk.Button(self.button_frame, text="Save")
+        save_button = tk.Button(self.button_frame, text='Save')
         save_button.pack(side=tk.RIGHT)
+        play_pause_button = tk.Button(self.button_frame, text='Play/Pause')
+        play_pause_button.pack(side=tk.RIGHT)
+        step_forward_button = tk.Button(self.button_frame, text='Step forward')
+        step_forward_button.pack(side=tk.RIGHT)
+        step_backward_button = tk.Button(self.button_frame, text='Step backward')
+        step_backward_button.pack(side=tk.RIGHT)
+        clear_button = tk.Button(self.button_frame, text='Clear')
+        clear_button.pack(side=tk.RIGHT)
+        self.speed_var = tk.IntVar(value=10)
+        self.speed_scale = tk.Scale(self.button_frame, label=f'{self.speed_var.get()}/s', showvalue=0, from_=1, to=100, orient=tk.HORIZONTAL, length=75, variable=self.speed_var)
+        def update_label(val):
+            self.speed_scale.config(label=f'{self.speed_var.get()}/s')
+        self.speed_scale.config(command=update_label)
+        self.speed_scale.set(self.speed_var.get())
+        self.speed_scale.pack(side=tk.RIGHT)
+        self.edit_button = tk.Button(self.button_frame, text='Edit')
+        self.edit_button.pack(side=tk.RIGHT)
 
 
     def load_pattern(self):
